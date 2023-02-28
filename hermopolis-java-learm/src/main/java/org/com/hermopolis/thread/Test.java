ca84ebe14b7c1edbaf6734c2bc5019deba7b2083
N7688 <lanxingrui@corp.netease.com>
2023-02-28 22:25:00 +0800

    add

--- hermopolis-java-learm/src/main/java/org/com/hermopolis/thread/Test.java
+++ hermopolis-java-learm/src/main/java/org/com/hermopolis/thread/Test.java
@@ -1,4 +1,29 @@
 package org.com.hermopolis.thread;
 
 public class Test {
+    static  class A {
+        static {
+            System.out.print("1");
+        }
+        public A() {
+            System.out.print("2");
+        }
+    }
+    static class B extends A{
+        static {
+            System.out.print("a");
+        }
+        public B() {
+           
+            System.out.print("b");
+        }
+    }
+    public static void main(String[] args) {
+        A ab = new B();
+      //  ab = new B();
+
+        ///  1a2b2b
+    }
+
+
 }
