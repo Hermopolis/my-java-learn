cbfd9fe52a67094ae76cbd7562981e595be547b0
N7688 <lanxingrui@corp.netease.com>
2023-03-16 09:37:30 +0800

    add

--- hermopolis-java-learm/src/main/java/org/com/hermopolis/socket/User.java
+++ hermopolis-java-learm/src/main/java/org/com/hermopolis/socket/User.java
@@ -8,6 +8,30 @@ import java.io.PrintWriter;
 public class User {
     private String uid;
     private PrintWriter out;
+   static  {
 
 
+    }
+
+    public User(String uid, PrintWriter out) {
+        this.uid = uid;
+        this.out = out;
+    }
+
+    {
+
+
+    }
+    public static void main(String[] args) {
+        String s = new String("sss").intern();
+        String s1 = new String("ss").intern();
+        s1.intern();
+        System.out.println(s == s1);
+        String s3 = s1.intern();
+        String s4 = s.intern();
+        System.out.println(s3 == s4);
+        System.out.println(s3.hashCode());
+        System.out.println(s.hashCode());
+        System.out.println(s1.hashCode());
+    }
 }
